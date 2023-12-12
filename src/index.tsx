294df407c8be5ee0afd5e16c6a94053596e42e28
Andrew Palmer <andrew.palmer.10@gmail.com>
2023-12-11 20:19:13 -0500

    Get rid of redux-persist (for now, at least)

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import { persistor, store } from './store'
+import { store } from './store'
 import './index.css'
-import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>,
 )
